a1c1043c25a1ddf371706bea99eea068194ebc8c
Cardullo Italia <cardullo.lia@gmail.com>
2023-11-06 09:22:14 +0100

    new project start
    inizio del progetto

---
+++ App-Profile-Studente/App-Profile-Studente/App_Profile_StudenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Profile_StudenteApp.swift
+//  App-Profile-Studente
+//
+//  Created by Italia Maria Desiré Cardullo on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct App_Profile_StudenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Profile-Studente/App-Profile-Studente/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-Profile-Studente
+//
+//  Created by Italia Maria Desiré Cardullo on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
